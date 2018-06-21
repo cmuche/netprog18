fac018a98036f8bde6f17404a19ae1fab91b83b2
Christoph Muche <christoph@muche.info>
2018-06-21 18:32:14 +0200

    +constants

--- netprog18_cmuche/Client.py
+++ netprog18_cmuche/Client.py
@@ -1,5 +1,6 @@
 import platform
 
+import Constants
 from netprog18 import ThinService
 
 from thrift import Thrift
@@ -9,6 +10,7 @@ from thrift.protocol import TBinaryProtocol
 
 from netprog18.ttypes import ClientInfo
 
+
 def getClientInfo():
     clientInfo = ClientInfo()
     clientInfo.cpu = platform.processor()
@@ -16,7 +18,8 @@ def getClientInfo():
     clientInfo.ram = "RAM"
     return clientInfo
 
-transport = TSocket.TSocket('127.0.0.1', 9090)
+
+transport = TSocket.TSocket(Constants.SERVER_HOST, Constants.SERVER_PORT)
 transport = TTransport.TBufferedTransport(transport)
 protocol = TBinaryProtocol.TBinaryProtocol(transport)
 client = ThinService.Client(protocol)
@@ -28,4 +31,3 @@ clientInfo = getClientInfo()
 client.hello(clientInfo)
 
 transport.close()
-
--- netprog18_cmuche/Server.py
+++ netprog18_cmuche/Server.py
@@ -1,3 +1,4 @@
+import Constants
 from netprog18 import ThinService
 
 from thrift.transport import TSocket
@@ -5,14 +6,16 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
+
 class ThinServiceHandler:
     def hello(self, clientInfo):
         print(clientInfo)
 
+
 print("CREATING SERVER")
 handler = ThinServiceHandler()
 processor = ThinService.Processor(handler)
-transport = TSocket.TServerSocket(host='127.0.0.1', port=9090)
+transport = TSocket.TServerSocket(Constants.SERVER_HOST, Constants.SERVER_PORT)
 tfactory = TTransport.TBufferedTransportFactory()
 pfactory = TBinaryProtocol.TBinaryProtocolFactory()
 
