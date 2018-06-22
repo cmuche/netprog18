5ee7afd88fd6b5c71464fe2af8eaf9dca9534675
Christoph Muche <christoph@muche.info>
2018-06-22 13:10:59 +0200

    clientconnector logging

--- netprog18_cmuche/ThinService/Client/ClientService.py
+++ netprog18_cmuche/ThinService/Client/ClientService.py
@@ -2,6 +2,7 @@ import random
 import sys
 
 from ThinService.Common import Constants
+from ThinService.Common.Logger import Logger
 from netprog18 import ThinService
 
 from thrift.transport import TSocket
@@ -11,7 +12,9 @@ from thrift.protocol import TBinaryProtocol
 
 class ClientConnector:
     def __init__(self):
+        self.logger = Logger("ClientConnector")
         self.id = random.randint(0, 2147483647)
+        self.logger.log("Client id: %d" % self.id)
 
     def connectToServer(self):
         self.transport = TSocket.TSocket(Constants.SERVER_HOST, Constants.SERVER_PORT)
