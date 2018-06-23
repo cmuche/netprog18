247ba131779953a9c572a86bcd97a0cbaedcd294
Christoph Muche <christoph@muche.info>
2018-06-23 10:47:35 +0200

    commandinterpreter: exception handling, param conversion, disable hello method

--- netprog18_cmuche/ThinService/Client/CommandInterpreter.py
+++ netprog18_cmuche/ThinService/Client/CommandInterpreter.py
@@ -1,6 +1,7 @@
 import sys
 
 from ThinService.Common.Logger import Logger
+from netprog18.ttypes import ClientAlreadyRegisteredError, InvalidClientId
 
 
 class CommandInterpreter:
@@ -11,13 +12,37 @@ class CommandInterpreter:
     def executeCommand(self, commandString):
         self.logger.log("Executing command '%s'" % commandString)
 
-        if commandString == "quit":
+        commandParts = commandString.split(" ")
+
+        if commandParts[0] == "quit":
             self.logger.log("Goodbye.")
             sys.exit()
 
+        if commandParts[0] == "hello":
+            self.logger.log("Can't execute 'hello' method manually!")
+            return
+
         try:
-            method = getattr(self.clientService, commandString)
-            result = method()
+            method = getattr(self.clientService, commandParts[0])
+            params = self.getParamsList(commandParts)
+            result = method(*params)
             print("Result = %s" % result)
+        except ClientAlreadyRegisteredError:
+            self.printError("A client with the same id is already registered on the server!")
+        except InvalidClientId:
+            self.printError("This client id does not exist!")
         except:
             self.logger.log("Invalid command!")
+
+    def printError(self, err):
+        print("Error = %s" % err)
+
+    def getParamsList(self, commandParts):
+        paramsRaw = commandParts[1:]
+        params = []
+        for r in paramsRaw:
+            try:
+                params.append(int(r))
+            except:
+                params.append(r)
+        return params
