3f94be8f0aa4e0682f2f9ea9e4ebbebd4e72df6b
Christoph Muche <christoph@muche.info>
2018-06-29 12:14:10 +0200

    +threshold constant

--- netprog18_cmuche/ThinService/Common/Constants.py
+++ netprog18_cmuche/ThinService/Common/Constants.py
@@ -1,5 +1,7 @@
 SERVER_HOST = "localhost"
 SERVER_PORT = 1337
 
+SERVER_LASTSEEN_THRESHOLD = 10
+
 CLIENT_PACKAGE_DIR = "client-package-tmp"
 CLIENT_PACKAGE_COMMAND = "unzip -o -q [dir]/[package] -d [dir]/[package]_out"
