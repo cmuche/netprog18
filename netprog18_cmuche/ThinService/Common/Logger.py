bdc87e33499efc1947958b2670d5ad344cfeeea5
Christoph Muche <christoph@muche.info>
2018-07-02 23:21:19 +0200

    common pydoc

--- netprog18_cmuche/ThinService/Common/Logger.py
+++ netprog18_cmuche/ThinService/Common/Logger.py
@@ -1,9 +1,22 @@
 class Logger:
     def __init__(self, senderName):
+        """
+        Initializes a logger
+        :param senderName: The name of the logging class
+        """
         self.senderName = senderName
 
     def log(self, message):
+        """
+        Logs a message
+        :param message: The message string
+        """
         print("[%s] %s" % (self.senderName, message))
 
     def logRequest(self, name, message):
+        """
+        Logs a API request
+        :param name: The name of the API method
+        :param message: The message string
+        """
         print("[%s] <%s> %s" % (self.senderName, name, message))
